c41541a9a2b155e745dcbf44e4da6ab048414ee1
sergeyleontiev <translatertrans@gmail.com>
2019-04-06 13:49:08 +0300

    Add draw circles Add Constants.swift

---
+++ TestRadar/Constants.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let numberCircles = 8
+let numberPlanes = 20
+
+let widthScreen = UIScreen.main.bounds.width
+let heightScreen = UIScreen.main.bounds.height
--- TestRadar/RadarViewController.swift
+++ TestRadar/RadarViewController.swift
@@ -2,8 +2,27 @@ import UIKit
 
 class RadarViewController: UIViewController {
 
+    var radius = CGFloat()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        radius = (widthScreen - 20) / CGFloat(2) / CGFloat(numberCircles)
+        for i in 1...numberCircles {
+            drawCircle(radius: radius * CGFloat(i))
+        }
+    }
+    
+    func drawCircle(radius: CGFloat) {
+        let centerScreen = CGPoint(x: widthScreen / 2, y: heightScreen / 2)
+        let circlePath = UIBezierPath(arcCenter: centerScreen, radius: radius, startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.gray.cgColor
+        
+        view.layer.addSublayer(shapeLayer)
     }
 
 }
